afa2f1202a26ce5751dcca87c26afcac69c17cdd
Frank Martinez <frank@lastmyle.co>
2024-11-29 16:03:43 -0800

    add advanced results placeholder

---
+++ app/advanced-results/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ExperimentScoringAdvancedStepFour() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      TODO: add results comparison component 
+    </div>
+  );
+}
